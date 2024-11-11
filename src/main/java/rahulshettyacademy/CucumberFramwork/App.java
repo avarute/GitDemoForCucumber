af34be905ee1ee9aa877c9b2d0b14c770a155d10
Amruta <amrutavarute22@gmail.com>
2024-11-12 00:13:08 +0530

    new branch added as newBranch

--- src/main/java/rahulshettyacademy/CucumberFramwork/App.java
+++ src/main/java/rahulshettyacademy/CucumberFramwork/App.java
@@ -3,5 +3,7 @@ package rahulshettyacademy.CucumberFramwork;
 public class App {
   public static void main(String[] args) {
     System.out.println("Hello World!");
+
+ System.out.println("Hello World123!");
   }
 }
